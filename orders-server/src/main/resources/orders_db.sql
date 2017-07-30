48264722c5e61ef1b5edecd4eb5e49d0eb14218e
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-07-30 16:32:18 +0300

    orders_db fix

--- orders-server/src/main/resources/orders_db.sql
+++ orders-server/src/main/resources/orders_db.sql
@@ -1,29 +1,27 @@
 CREATE DATABASE order_database;
-CREATE USER order_user WITH password 'qwerty';
+CREATE USER order_user WITH password 'order_user';
 GRANT ALL ON DATABASE order_database TO order_user;
 
 CREATE TYPE CLIENT_TYPE AS ENUM ('CUSTOMER','EXECUTOR');
 
-CREATE TABLE order (
-  id SERIAL PRIMARY KEY,
-  id_customer integer REFERENCES users,
-  id_executor REFERENCES users,
-  address CHAR(64),
-  description VARCHAR(64),
-  coordinates point);
-
-CREATE TABLE client (
-	id SERIAL PRIMARY KEY,
-	id_personal_information integer REFERENCES personal_information,
-	client_type CLIENT_TYPE NOT NULL,
-	coordinates point;
-);
-
 CREATE TABLE personal_information (
-	id SERIAL PRIMARY KEY,
+	id bigserial PRIMARY KEY,
 	first_name VARCHAR(64) NOT NULL,
     last_name VARCHAR(64) NOT NULL,
     middle_name VARCHAR(64),
     identifier VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL;
-);
+    password VARCHAR(255) NOT NULL);
+
+CREATE TABLE clients (
+	id bigserial PRIMARY KEY,
+	id_personal_information bigserial REFERENCES personal_information,
+	client_type CLIENT_TYPE NOT NULL,
+	coordinates point);
+
+CREATE TABLE orders (
+  id bigserial PRIMARY KEY,
+  id_customer bigserial REFERENCES clients,
+  id_executor bigserial REFERENCES clients,
+  address CHAR(64),
+  description VARCHAR(64),
+  coordinates point);
