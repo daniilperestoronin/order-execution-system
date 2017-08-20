175d051361a121d940b1c37d578d512fae5a4f44
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-08-20 19:23:59 +0300

    UserServices added

--- authentication-server/src/main/java/com/orderexecution/authentication/service/UserService.java
+++ authentication-server/src/main/java/com/orderexecution/authentication/service/UserService.java
@@ -1,7 +1,13 @@
 package com.orderexecution.authentication.service;
 
+import com.orderexecution.authentication.domainobjects.User;
+
 /**
  * @author Perestoronin Daniil
  */
 public interface UserService {
+
+    User save(User user);
+
+    User findByEmail(String email);
 }
--- authentication-server/src/main/java/com/orderexecution/authentication/service/impl/UserServiceImpl.java
+++ authentication-server/src/main/java/com/orderexecution/authentication/service/impl/UserServiceImpl.java
@@ -1,7 +1,25 @@
 package com.orderexecution.authentication.service.impl;
 
+import com.orderexecution.authentication.domainobjects.User;
+import com.orderexecution.authentication.repository.UserRepository;
+import com.orderexecution.authentication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author Perestoronin Daniil
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
