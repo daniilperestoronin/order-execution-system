8a9d227e0767dd160e927f1c9a43729d3e27fad3
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-08-21 23:19:42 +0300

    SpringBoot app added

---
+++ authentication-server/src/main/java/com/orderexecution/authentication/AuthenticationServerApplication.java
@@ -0,0 +1,14 @@
+package com.orderexecution.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Perestoronin Daniil
+ */
+@SpringBootApplication
+public class AuthenticationServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServerApplication.class, args);
+    }
+}
