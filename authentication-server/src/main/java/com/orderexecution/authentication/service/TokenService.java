92377e81cf3798ded4c2623187c7a1a9813185be
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-08-29 17:41:21 +0300

    TokenService added

---
+++ authentication-server/src/main/java/com/orderexecution/authentication/service/TokenService.java
@@ -0,0 +1,10 @@
+package com.orderexecution.authentication.service;
+
+/**
+ * Created by perestoronin on 29.08.2017.
+ */
+public interface TokenService {
+
+
+
+}
---
+++ authentication-server/src/main/java/com/orderexecution/authentication/service/impl/TokenServiceImpl.java
@@ -0,0 +1,9 @@
+package com.orderexecution.authentication.service.impl;
+
+import com.orderexecution.authentication.service.TokenService;
+
+/**
+ * Created by perestoronin on 29.08.2017.
+ */
+public class TokenServiceImpl implements TokenService {
+}
