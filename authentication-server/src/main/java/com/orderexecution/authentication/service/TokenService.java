8f8b4d5e66ea62699f9e6b463924cafa871c5c74
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-09-06 23:35:45 +0300

    TokenService change

--- authentication-server/src/main/java/com/orderexecution/authentication/service/TokenService.java
+++ authentication-server/src/main/java/com/orderexecution/authentication/service/TokenService.java
@@ -1,10 +1,12 @@
 package com.orderexecution.authentication.service;
 
+import com.orderexecution.authentication.domainobjects.User;
+
 /**
  * Created by perestoronin on 29.08.2017.
  */
 public interface TokenService {
+    boolean checkToken(String token);
 
-
-
+    String generateToken(User user);
 }
--- authentication-server/src/main/java/com/orderexecution/authentication/service/impl/TokenServiceImpl.java
+++ authentication-server/src/main/java/com/orderexecution/authentication/service/impl/TokenServiceImpl.java
@@ -1,9 +1,28 @@
 package com.orderexecution.authentication.service.impl;
 
+import com.orderexecution.authentication.domainobjects.User;
 import com.orderexecution.authentication.service.TokenService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * Created by perestoronin on 29.08.2017.
  */
+@Service
 public class TokenServiceImpl implements TokenService {
+
+    @Override
+    public boolean checkToken(String token) {
+        return false;
+    }
+
+    @Override
+    public String generateToken(User user) {
+        return Jwts.builder().setSubject(user.getEmail()).claim("roles", "user").setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+    }
+
 }
