5f3a97f5f301dedd6822d5044fb6ca7a4710fb91
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-09-06 23:22:15 +0300

    TokenController change

--- authentication-server/src/main/java/com/orderexecution/authentication/controller/TokenController.java
+++ authentication-server/src/main/java/com/orderexecution/authentication/controller/TokenController.java
@@ -1,7 +1,24 @@
 package com.orderexecution.authentication.controller;
 
+import com.orderexecution.authentication.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by perestoronin on 29.08.2017.
  */
+@RestController
+@RequestMapping("/token")
 public class TokenController {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping(value = "/check")
+    public boolean checkToken(@RequestBody String token) {
+        return tokenService.checkToken(token);
+    }
 }
