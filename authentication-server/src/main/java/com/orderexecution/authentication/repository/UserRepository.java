bdd696603b08d0319b0da34836523022d647b92b
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-09-06 23:36:17 +0300

    UserRepository change

--- authentication-server/src/main/java/com/orderexecution/authentication/repository/UserRepository.java
+++ authentication-server/src/main/java/com/orderexecution/authentication/repository/UserRepository.java
@@ -9,6 +9,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User save(User user);
-
     User findByEmail(String email);
 }
