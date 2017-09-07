2d0af3f48fa75378a06252e103cfc63a14ce5adb
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-09-07 23:47:32 +0300

    TokenService update

--- authentication-server/src/main/java/com/orderexecution/authentication/service/impl/TokenServiceImpl.java
+++ authentication-server/src/main/java/com/orderexecution/authentication/service/impl/TokenServiceImpl.java
@@ -1,9 +1,11 @@
 package com.orderexecution.authentication.service.impl;
 
 import com.orderexecution.authentication.domainobjects.User;
+import com.orderexecution.authentication.repository.TokenRepository;
 import com.orderexecution.authentication.service.TokenService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -14,15 +16,20 @@ import java.util.Date;
 @Service
 public class TokenServiceImpl implements TokenService {
 
+    @Autowired
+    private TokenRepository tokenRepository;
+
     @Override
     public boolean checkToken(String token) {
-        return false;
+        return tokenRepository.checkToken(token) != null ? true : false;
     }
 
     @Override
     public String generateToken(User user) {
-        return Jwts.builder().setSubject(user.getEmail()).claim("roles", "user").setIssuedAt(new Date())
+        String token = Jwts.builder().setSubject(user.getEmail()).claim("roles", "user").setIssuedAt(new Date())
                 .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+        tokenRepository.addToken(token, user);
+        return token;
     }
 
 }
