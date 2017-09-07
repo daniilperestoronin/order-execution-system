478aa9122229b3ff7f3d293065d00181b32ef8c6
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-09-07 23:41:06 +0300

    RedisConfiguration added

---
+++ authentication-server/src/main/java/com/orderexecution/authentication/configuration/RedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.orderexecution.authentication.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author Perestoronin Daniil
+ */
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
+        jedisConFactory.setHostName("localhost");
+        jedisConFactory.setPort(6379);
+        return jedisConFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
