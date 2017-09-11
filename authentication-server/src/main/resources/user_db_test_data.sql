8c3d5543cc45d75623b4da212b44e4db67bd5a67
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-09-11 23:03:04 +0300

    User db sql scripts change

--- authentication-server/src/main/resources/user_db_test_data.sql
+++ authentication-server/src/main/resources/user_db_test_data.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (first_name, last_name, middle_name, identifier, password)
+INSERT INTO users (first_name, last_name, middle_name, client_type,identifier, password)
 VALUES
-  ('Alan', 'Abbot', 'S', 'alan_abbot@gmail.com', 'alan_abbot','CUSTOMER'),
-  ('Bob', 'Corey', 'A', 'bob_corey@gmail.com', 'bob_corey','EXECUTOR'),
-  ('Jack', 'McCorty', 'M', 'jack_mccorty@gmail.com', 'jack_mccorty','EXECUTOR');
+  ('Alan', 'Abbot', 'S','CUSTOMER', 'alan_abbot@gmail.com', 'alan_abbot'),
+  ('Bob', 'Corey', 'A','EXECUTOR', 'bob_corey@gmail.com', 'bob_corey'),
+  ('Jack', 'McCorty', 'M','ADMINISTRATOR', 'jack_mccorty@gmail.com', 'jack_mccorty');
 
