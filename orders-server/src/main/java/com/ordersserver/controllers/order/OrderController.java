30cc0982978f47928e4c35e614a4e44b5036cd5f
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-11-24 23:26:38 +0300

    Order controller fix

--- orders-server/src/main/java/com/ordersserver/controllers/order/OrderController.java
+++ orders-server/src/main/java/com/ordersserver/controllers/order/OrderController.java
@@ -18,43 +18,36 @@ public class OrderController {
     OrderService orderService;
 
     @GetMapping("/{id}")
-    @ResponseBody
     public Order getOrderById(@PathVariable Long id) {
         return orderService.retrieve(id);
     }
 
     @PostMapping("/")
-    @ResponseBody
     public void createOrder(@RequestBody Order order) {
         orderService.create(order);
     }
 
     @PutMapping("/")
-    @ResponseBody
     public void updateOrderById(@RequestBody Order order) {
         orderService.update(order);
     }
 
     @DeleteMapping("/{id}")
-    @ResponseBody
     public void deleteOrder(@PathVariable Long id) {
         orderService.delete(id);
     }
 
     @GetMapping("/all")
-    @ResponseBody
     public Collection<Order> getOrderById() {
         return orderService.getAllOrders();
     }
 
     @GetMapping("/unexecuted")
-    @ResponseBody
     public Collection<Order> getUnexecutedOrders() {
         return orderService.getUnexecutedOrders();
     }
 
     @GetMapping("/executed")
-    @ResponseBody
     public Collection<Order> getExecutedOrders() {
         return orderService.getExecutedOrders();
     }
